a5f00460c2eb8758d0945ee700f44c2b24a8f2b5
LMY <lmun768@naver.com>
2020-09-01 19:05:11 +0900

    [ADD] RetrofitClientWeather [UPDATE] DAO, Manifest

---
+++ Iot_Android/app/src/main/java/com/example/iot_android/retrofit/RetrofitClientWeather.kt
@@ -0,0 +1,18 @@
+package com.example.iot_android.retrofit
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClientWeather {
+    private var instance : Retrofit? = null
+
+    fun getInstance() : Retrofit {
+        if(instance == null){
+            instance = Retrofit.Builder()
+                .baseUrl("https://api.openweathermap.org/data/2.5")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+        return instance!!
+    }
+}
