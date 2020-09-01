d1c7bc5a84de5144b3f30f38340e3fb7f05ce859
LMY <lmun768@naver.com>
2020-09-01 09:10:00 +0900

    [UPDATE] LoginMain

--- Iot_Android/app/src/main/java/com/example/iot_android/viewModel/LoginMainViewModel.kt
+++ Iot_Android/app/src/main/java/com/example/iot_android/viewModel/LoginMainViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.iot_android.viewModel
 
 import androidx.lifecycle.ViewModel
+import com.example.iot_android.widget.SingleLiveEvent
 
 class LoginMainViewModel () : ViewModel(){
+
 
 }
