0faa5ffc559835ba05b2ce5b426450d8a782c360
LMY <lmun768@naver.com>
2020-09-04 14:17:17 +0900

    [UPDAtE] Weather

--- Iot_Android/app/src/main/java/com/example/iot_android/viewModel/WeatherViewModel.kt
+++ Iot_Android/app/src/main/java/com/example/iot_android/viewModel/WeatherViewModel.kt
@@ -1,12 +1,37 @@
 package com.example.iot_android.viewModel
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.iot_android.model.LoginBody
+import com.example.iot_android.model.LoginData
+import com.example.iot_android.model.weather.WeatherData
+import com.example.iot_android.retrofit.InterfaceService
+import com.example.iot_android.widget.MyApplication
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 
 class WeatherViewModel : ViewModel() {
 
-    fun getLocation(){
+    lateinit var latitude : MutableLiveData<Double>
+    lateinit var longitude : MutableLiveData<Double>
 
+    lateinit var myAPI : InterfaceService
+    lateinit var retrofit: Retrofit
+
+    fun getWeather(){
+        myAPI = retrofit.create(InterfaceService::class.java)
+        myAPI.getWeather(lat = latitude.value.toString(), lon = longitude.value.toString()).enqueue(object :
+            Callback<WeatherData> {
+            override fun onFailure(call: Call<WeatherData>, t: Throwable) {
+            }
+            override fun onResponse(call: Call<WeatherData>, response: Response<WeatherData>) {
+            }
+        })
     }
+
 
 
 }
