b4ecd3ebaf1c31567371e0b315817a6bfc12af07
LMY <lmun768@naver.com>
2020-09-17 20:32:14 +0900

    [UPDATE] Weather

--- Iot_Android/app/src/main/java/com/example/iot_android/view/fragment/WeatherFragment.kt
+++ Iot_Android/app/src/main/java/com/example/iot_android/view/fragment/WeatherFragment.kt
@@ -1,26 +1,25 @@
 package com.example.iot_android.view.fragment
 
-import android.app.AlertDialog
-import android.content.Context
-import android.content.DialogInterface
-import android.content.Intent
-import android.location.LocationManager
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
-import android.provider.Settings
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.iot_android.R
 import com.example.iot_android.databinding.FragmentWeatherBinding
-import com.example.iot_android.retrofit.RetrofitClient
 import com.example.iot_android.retrofit.RetrofitClientWeather
 import com.example.iot_android.viewModel.WeatherViewModel
 import com.example.iot_android.widget.GpsTracker
+import com.example.iot_android.widget.extension.toast
+import kotlinx.android.synthetic.main.fragment_weather.*
+import java.io.IOException
+import java.util.*
 
 class WeatherFragment : Fragment() {
 
@@ -53,7 +52,13 @@ class WeatherFragment : Fragment() {
         mViewModel.latitude.value = gpsTracker.getLatitude().toInt()
         mViewModel.longitude.value = gpsTracker.getLongitude().toInt()
 
+        val address = getCurrentAddress(gpsTracker.getLatitude(), gpsTracker.getLongitude())
+        locate.text = address
+
+
         mViewModel.retrofit = RetrofitClientWeather.getInstance()
+
+
     }
 
 
@@ -71,4 +76,34 @@ class WeatherFragment : Fragment() {
             })
         }
     }
+
+    fun getCurrentAddress(latitude: Double, longitude: Double): String? {
+
+        val geocoder = Geocoder(activity, Locale.getDefault())
+        val addresses: List<Address>
+        addresses = try {
+            geocoder.getFromLocation(
+                latitude,
+                longitude,
+                7
+            )
+        } catch (ioException: IOException) {
+            //네트워크 문제
+            toast("지오코더 서비스 사용불가")
+            return "지오코더 서비스 사용불가"
+        } catch (illegalArgumentException: IllegalArgumentException) {
+            toast("잘못된 GPS 좌표")
+            return "잘못된 GPS 좌표"
+        }
+        if (addresses == null || addresses.size == 0) {
+            toast("주소 미발견")
+            return "주소 미발견"
+        }
+        val address: Address = addresses[0]
+
+        return address.adminArea.replace("광역시", "")
+    }
+
+
+
 }
