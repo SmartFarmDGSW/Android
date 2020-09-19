4bc9a2c006955563ebd0c03038f00c7f565c7b88
LMY <lmun768@naver.com>
2020-09-19 19:31:43 +0900

    [UPDATE] WeatherRecylerView

--- Iot_Android/app/src/main/java/com/example/iot_android/adapter/WeatherAdapter.kt
+++ Iot_Android/app/src/main/java/com/example/iot_android/adapter/WeatherAdapter.kt
@@ -6,11 +6,14 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.iot_android.R
+import com.example.iot_android.model.weather.Daily
 import com.example.iot_android.model.weather.WeatherData
+import com.example.iot_android.service.utils.DateManager
+import java.time.Instant
+import java.time.LocalDateTime
+import java.util.*
 
-class WeatherAdapter(val weatherList : ArrayList<WeatherData>) : RecyclerView.Adapter<WeatherAdapter.Holder>() {
-
-    var count = 1
+class WeatherAdapter(val weatherList : WeatherData) : RecyclerView.Adapter<WeatherAdapter.Holder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.weather_item, parent, false)
@@ -18,21 +21,20 @@ class WeatherAdapter(val weatherList : ArrayList<WeatherData>) : RecyclerView.Ad
     }
 
     override fun getItemCount(): Int {
-        return weatherList.size
+        return weatherList.daily.size - 1
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.bind(weatherList[position])
+        holder.bind(weatherList.daily[position + 1])
     }
 
     inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
         val temp = itemView.findViewById<TextView>(R.id.textView38)
-
-        fun bind(weather: WeatherData) {
-            if(count < 8) {
-                temp.text = weather.daily.get(count++).toString()
-            }
+        val day = itemView.findViewById<TextView>(R.id.textView40)
+        fun bind(weather: Daily) {
+            temp.text = weather.temp.day.toString()
+            day.text = DateManager.getDate(LocalDateTime.ofInstant(Instant.ofEpochMilli(weather.dt * 1000), TimeZone.getDefault().toZoneId()))
         }
     }
 }
