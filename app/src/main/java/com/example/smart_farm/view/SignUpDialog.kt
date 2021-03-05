c361b6db44853096811a3c9d104471d5a016e69b
yangjunhyuk333 <chuchoun1234@gmail.com>
2021-03-05 10:58:10 +0900

    [UPDATE]Move pakcge [UPDATE]Chage View Id

---
+++ app/src/main/java/com/example/smart_farm/view/SignUpDialog.kt
@@ -0,0 +1,39 @@
+package com.example.smart_farm.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.smart_farm.R
+import com.example.smart_farm.databinding.DialogSigninBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class SignUpDialog : BottomSheetDialogFragment() {
+
+    private lateinit var dataBinding: DialogSigninBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.CustomBottomSheetDialogTheme)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        val signUpDialog = SignUpDialog()
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.dialog_signin, container, false)
+
+        with(dataBinding){
+            signUp.setOnClickListener {
+                signUpDialog.show(fragmentManager!!, "signUpDialog")
+                dialog?.dismiss()
+            }
+        }
+
+        return dataBinding.root
+    }
+}
